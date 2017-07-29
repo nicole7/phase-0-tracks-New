e60e5462de1d50243f723942536bedbf0a95c188
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-07-29 12:56:07 -0500

    Tried out different ways to get and change data

--- nested_data_structures.rb
+++ nested_data_structures.rb
@@ -1,41 +1,54 @@
 highway = {
-	north_bound: => {
-		pretty_name: => "North Bound",
-		blue_car: => {
+	north_bound: {
+		pretty_name: 'North Bound',
+		blue_car: {
 			total_people: 4,
-			destination: "Madison, WI"
-		}
-		white_truck: => {
+			destination: 'Madison, WI'
+		},
+		white_truck: {
 			total_people: 1,
-			destination: "Chicago, IL"
-		}
+			destination: 'Chicago, IL'
+		},
 		
-		grey_suv: => {
+		grey_suv: {
 			total_people: 5,
-			destination: "Wilmette, IL"
-		}
-		traffic_type: => [
-			"Slow"
+			destination: 'Wilmette, IL'
+		},
+		traffic_and_weather: [
+			'Slow', 'Rainy'
 		]
-	}
-	south_bound: => {
-		pretty_name: => "South Bound"
-		yellow_sports_car: => {
-			total_people: 1,
-			destination: "Joliet"
-		}
-		green_jeep: => {
-			total_people: 3,
-			destination: Bloomington, IL
-		}
-		black_semi: => {
-			total_people: 1,
-			destination: "St, Louis, MO"
-		}
-		traffic_type: => 
-			"Moderate"
-		]
-	}
-}
+	},#Close for north_bound
 
-puts highway[north_bound:][blue_car:]
+	#Don't forget the commas in between to separate everything out!
+	#Don't need => when you have :
+
+	south_bound: {
+		pretty_name: 'South Bound',
+		yellow_sports_car: {
+			total_people: 1,
+			destination: 'Joliet'
+		},
+		green_jeep: {
+			total_people: 3,
+			destination: 'Bloomington, IL'
+		},
+		black_semi: {
+			total_people: 1,
+			destination: 'St, Louis, MO'
+		},
+		traffic_and_weather: [
+			'Moderate', 'Rainy'
+		]
+	}#Close for south_bound
+}#Close for highway hash
+
+#Prints the hash for the blue car that includes how many ppl in it and destination
+p highway[:north_bound][:blue_car]
+
+#Changes Slow at index 0 to Moderate
+highway[:north_bound][:traffic_and_weather][0] = "Moderate"
+p highway[:north_bound][:traffic_and_weather]
+
+#Picked up a hitchhiker
+highway[:south_bound][:green_jeep] = 4
+puts highway[:south_bound][:green_jeep]
