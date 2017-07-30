e3be6a5f80464f5195987bcd93d846627ffe8608
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-07-29 20:24:12 -0500

    Solved fib method

--- problem_solving_methods.rb
+++ problem_solving_methods.rb
@@ -20,3 +20,30 @@ end
 arr = [3, 65, 9, 10, 17, 24]
 #Call to method, pass this array and a number to search for
 search_array(arr, 10)
+
+p ".............................."
+
+#Create a Fibonacci method by starting off with 0 and 1
+#Keep adding numbers onto the list by adding the previous two
+#Array of 6 should look like: [0, 1, 1, 2, 3, 5]
+#Each number in the array us the sum of the previous two numbers, except for the 
+#0 and the 1
+#Method will return an array
+
+def create_fib(num)
+	num1 = 0
+	num2 = 1
+	fib_array = []
+	
+	index = 0
+	while index <= num
+		temp = num1
+		num1 = num2
+		num2 = temp + num1
+		fib_array << num1
+		index += 1
+	end
+	fib_array
+end
+
+p create_fib(11)
