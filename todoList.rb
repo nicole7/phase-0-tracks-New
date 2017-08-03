80fc12f2fa2b03e3a674233f86d01899b594b7bb
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-08-02 20:53:36 -0500

    Complete rspec

---
+++ todoList.rb
@@ -0,0 +1,22 @@
+class TodoList
+	def initialize(list)
+		@list = list
+	end
+
+	def get_items
+		@list
+	end
+
+	def add_item(item)
+		@list << item
+	end
+
+	def delete_item(item)
+		@list.delete(item)
+	end
+
+	def get_item
+		@list[0]
+	end
+
+end
