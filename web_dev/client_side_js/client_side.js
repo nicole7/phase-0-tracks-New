db14f24b41ea151dee7d39e6f1de23fe4190cd4b
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-08-27 18:17:42 -0500

    complete

--- web_dev/client_side_js/client_side.js
+++ web_dev/client_side_js/client_side.js
@@ -4,10 +4,8 @@ var bold_el = el.bold();
 bold_el.style.color = 'green';
 
 
-function displayImage1() {
-	document.getElementById('viewBandit1').addEventListener("click", src="bandit.png");
-}
+function hide() {
+	document.getElementById('button').addEventListener("click",alert("Hiding!"))
+	};
 
-function displayImage2() {
-	document.getElementById('viewBandit2').addEventListener("click", src="banditSleeps.png");
-}
+
