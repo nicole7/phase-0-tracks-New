123e29a490dbe65e4aa6990e2e17c2d41cb71edf
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-08-27 19:21:43 -0500

    Made updates

--- web_dev/client_side_js/client_side.js
+++ web_dev/client_side_js/client_side.js
@@ -3,9 +3,27 @@ var bold_el = el.bold();
 
 bold_el.style.color = 'green';
 
+function image1Hide(){
+var banditHide = document.getElementById("bandit_hide");
+banditHide.style.display = 'none';
+};
 
-function hide() {
-	document.getElementById('button').addEventListener("click",alert("Hiding!"))
+function hideImage() {
+	document.getElementById('button1').addEventListener("click", image1Hide);
 	};
+	
+function image2Sleep(){
+var banditSleep = document.getElementById("bandit_sleep");
+banditSleep.style.opacity = -1;
+};
+
+function fadeImage() {
+	document.getElementById('button2').addEventListener("click", image2Sleep);
+	
+	};
+	
+
+	
+
 
 
